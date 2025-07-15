024d476ef27bc5c8cf969ac63ccdc51bbc38f2dc
Ravindra Dayaram Bagul <132358935+Ravindra-Bagul@users.noreply.github.com>
2025-07-16 01:04:51 +0530

    Update api.js

--- auction-frontend/src/services/api.js
+++ auction-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://bidsphere-production.up.railway.app/';
 
 const handleError = (error) => {
     if (error.response) {
